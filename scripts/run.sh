f7751237dda7adbc392720374178d63ceb14ac31
janitha000 <JanithaT@99x.lk>
2019-06-26 14:16:18 +0530

    modified script files

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,4 +4,4 @@ if [ ! -z "$DEPLOYMENT_GROUP_NAME" ]; then
 fi
 
 cd ~/TaxiMicroServiceNode
-pm2 start server
+/home/ec2-user/.nvm/versions/node/v10.15.1/lib/node_modules/pm2/bin/pm2 start server
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd ~/TaxiMicroServiceNode
-pm2 stop server
+/home/ec2-user/.nvm/versions/node/v10.15.1/lib/node_modules/pm2/bin/pm2  stop server
