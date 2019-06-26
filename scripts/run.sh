63c63695700a0ed612c1a616a87f557aa48954b1
janitha000 <JanithaT@99x.lk>
2019-06-26 15:53:24 +0530

    modified app-stop.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,4 @@
-#!/usr/bin/bash
-if [ ! -z "$DEPLOYMENT_GROUP_NAME" ]; then
- export NODE_ENV=$DEPLOYMENT_GROUP_NAME
-fi
+#!/bin/bash
 
 cd ~/TaxiMicroServiceNode
 /home/ec2-user/.nvm/versions/node/v10.15.1/lib/node_modules/pm2/bin/pm2 start server
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,2 +1,2 @@
-#!/usr/bin/bash
+#!/bin/bash
 /home/ec2-user/.nvm/versions/node/v10.15.1/lib/node_modules/pm2/bin/pm2  stop server
